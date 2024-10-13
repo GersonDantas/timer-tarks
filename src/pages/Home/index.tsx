e524e2c67ea8505e1842e014255cd6b797cccb5c
gersondantas <gerson.accari@hotmail.com>
2024-10-13 15:44:39 -0300

    refactor: move code to a helpers components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,51 +5,32 @@ import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import * as zod from 'zod'
 
-import { } from './styles'
+import { Countdown } from './components/Countdown'
+import { NewCycleForm } from './components/NewCycleForm'
 import {
-  CountDownContainer,
-  FormContainer,
   HomeContainer,
-  MinutesAmountInput,
-  Separator,
   StartCountdownButton,
   StopCountdownButton,
-  TaskInput,
 } from './styles'
-
-const newCycleValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa!'),
-  minutesAmount: zod
-    .number()
-    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
-    .max(60, 'O ciclo precisa ser de no máximo 5 minutos.'),
-})
-
-type NewCycleFormData = zod.infer<typeof newCycleValidationSchema>
 
 interface Cycle {
   id: string
   task: string
-  minutesAmount: number,
-  startDate: Date,
+  minutesAmount: number
+  startDate: Date
   interruptedDate?: Date
+  finishedDate?: Date
 }
 
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-    resolver: zodResolver(newCycleValidationSchema),
-    defaultValues: {
-      task: '',
-      minutesAmount: 0,
-    },
-  })
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime())
+
     const newCycle: Cycle = {
       id,
       task: data.task,
@@ -57,7 +38,7 @@ export function Home() {
       startDate: new Date(),
     }
 
-    setCycles((oldValue: Cycle[]) => [...oldValue, newCycle])
+    setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
 
@@ -65,38 +46,17 @@ export function Home() {
   }
 
   function handleInterruptCycle() {
-    setCycles(
-      cycles.map((cycle: Cycle) => {
+    setCycles((state) =>
+      state.map((cycle) => {
         if (cycle.id === activeCycleId) {
           return { ...cycle, interruptedDate: new Date() }
         } else {
           return cycle
         }
-      }))
-
+      }),
+    )
     setActiveCycleId(null)
   }
-
-  const activeCycle = cycles.find(({ id }) => id === activeCycleId)
-
-  useEffect(() => {
-    let interval: number
-    if (activeCycle) {
-      interval = setInterval(() => {
-        setAmountSecondsPassed(
-          differenceInSeconds(new Date(), activeCycle.startDate),
-        )
-      }, 1000)
-    }
-
-    return () => {
-      clearInterval(interval)
-    }
-  }, [activeCycle])
-
-  const totalSeconds = activeCycle
-    ? activeCycle.minutesAmount * 60
-    : 0
 
   const currentSeconds = activeCycle
     ? totalSeconds - amountSecondsPassed
@@ -112,55 +72,20 @@ export function Home() {
     if (activeCycle) {
       document.title = `${minutes}:${seconds}`
     }
-  }, [activeCycle, minutes, seconds])
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisable = !task
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
-        <FormContainer>
-          <label htmlFor="task">vou trabalhar em</label>
-          <TaskInput
-            type="text"
-            id="task"
-            list="task-suggestions"
-            disabled={!!activeCycle}
-            placeholder="Dê um nome para seu projeto"
-            {...register('task')}
-          />
-
-          <datalist id="task-suggestions">
-            <option value="Projeto 1" />
-            <option value="Projeto 1" />
-            <option value="Projeto 1" />
-            <option value="Projeto 1" />
-            <option value="Projeto 1" />
-          </datalist>
-
-          <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput
-            type="number"
-            id="minutesAmount"
-            placeholder="00"
-            step={5}
-            disabled={!!activeCycle}
-            min={5}
-            max={60}
-            {...register('minutesAmount', { valueAsNumber: true })}
-          />
-
-          <span>minutos.</span>
-        </FormContainer>
-
-        <CountDownContainer>
-          <span>{minutes[0]}</span>
-          <span>{minutes[1]}</span>
-          <Separator>:</Separator>
-          <span>{seconds[0]}</span>
-          <span>{seconds[1]}</span>
-        </CountDownContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
+        <NewCycleForm />
+        <Countdown
+          activeCycle={activeCycle}
+          setCycles={setCycles}
+          activeCycleId={activeCycleId}
+        />
 
         {activeCycle
           ? (
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -7,7 +7,7 @@ export const HomeContainer = styled.main`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  
+
   form {
     display: flex;
     flex-direction: column;
@@ -16,107 +16,41 @@ export const HomeContainer = styled.main`
   }
 `
 
-export const FormContainer = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: .5rem;
-  color: ${({ theme }) => theme['gray-100']};
-  font-size: 1.125rem;
-  font-weight: bold;
-  flex-wrap: wrap;
-`
-
-export const CountDownContainer = styled.button`
-  font-family: 'Robot Mono', monospace;
-  font-size: 10rem;
-  line-height: 8rem;
-  color: ${({ theme }) => theme['gray-700']};
-
-  display: flex;
-  gap: 1rem;
-
-  span {
-    background: ${({ theme }) => theme['gray-700']};
-    padding: 2rem 1rem;
-    border-radius: 8px;
-  }
-`
-
-export const Separator = styled.div`
-  padding: 2rem 0;
-  color: ${({ theme }) => theme['gray-500']};
-
-  width: 4rem;
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-`
-
-const BaseCountDownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
   border-radius: 8px;
+
   display: flex;
   align-items: center;
   justify-content: center;
+  color: ${(props) => props.theme['gray-100']};
 
-  gap: .5rem;
+  gap: 0.5rem;
   font-weight: bold;
 
   cursor: pointer;
 
-  color: ${({ theme }) => theme['gray-100']};
-
   &:disabled {
-    opacity: .7;
+    opacity: 0.7;
     cursor: not-allowed;
   }
 `
 
-export const StopCountdownButton = styled(BaseCountDownButton)`
-  background-color: ${({ theme }) => theme['red-500']};
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
 
   &:not(:disabled):hover {
-    background-color: ${({ theme }) => theme['red-700']};
+    background: ${(props) => props.theme['green-700']};
   }
 `
-export const StartCountdownButton = styled(BaseCountDownButton)`
-  background-color: ${({ theme }) => theme['green-500']};
-  
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+
   &:not(:disabled):hover {
-    background-color: ${({ theme }) => theme['green-700']};
+    background: ${(props) => props.theme['red-700']};
   }
-`
-
-const BaseInput = styled.input`
-  background: transparent;
-  height: 2.5rem;
-  border: 0;
-  border-bottom: 2px solid ${({ theme }) => theme['gray-500']};
-  font-weight: 1.125rem;
-  padding: 0 0.5rem;
-  color: ${({ theme }) => theme['gray-100']};
-
-  &:focus {
-    box-shadow: none;
-    border-color: ${({ theme }) => theme['green-500']};
-  }
-
-  ::placeholder {
-    color: ${({ theme }) => theme['gray-500']};
-  }
-`
-
-export const TaskInput = styled(BaseInput)`
-flex: 1;
-
-&::-webkit-calendar-picker-indicator {
-  display: none !important;
-}
-`
-export const MinutesAmountInput = styled(BaseInput)`
-  width: 4rem;
 `
