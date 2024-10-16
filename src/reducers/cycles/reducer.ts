caf2c2d3666d0795e56e6b85319169811e527b17
gersondantas <gerson.accari@hotmail.com>
2024-10-16 20:12:15 -0300

    refactor: move code to a helper method

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -16,41 +16,34 @@ interface CyclesState {
   activeCycleId: string | null
 }
 
+type PropertyProps = 'finishedDate' | 'interruptedDate'
+
 export function cyclesReducer(state: CyclesState, action: any) {
+  const setPropertyToNewDate = (property: PropertyProps) => {
+    const currentCycleIndex = state.cycles.findIndex((cycle) => {
+      return cycle.id === state.activeCycleId
+    })
+
+    if (currentCycleIndex < 0) {
+      return state
+    }
+
+    return produce(state, (draft) => {
+      draft.activeCycleId = null
+      draft.cycles[currentCycleIndex][property] = new Date()
+    })
+  }
+
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE:
       return produce(state, (draft) => {
         draft.cycles.push(action.payload.newCycle)
         draft.activeCycleId = action.payload.newCycle.id
       })
-    case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
-      const currentCycleIndex = state.cycles.findIndex((cycle) => {
-        return cycle.id === state.activeCycleId
-      })
-
-      if (currentCycleIndex < 0) {
-        return state
-      }
-
-      return produce(state, (draft) => {
-        draft.activeCycleId = null
-        draft.cycles[currentCycleIndex].interruptedDate = new Date()
-      })
-    }
-    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
-      const currentCycleIndex = state.cycles.findIndex((cycle) => {
-        return cycle.id === state.activeCycleId
-      })
-
-      if (currentCycleIndex < 0) {
-        return state
-      }
-
-      return produce(state, (draft) => {
-        draft.activeCycleId = null
-        draft.cycles[currentCycleIndex].finishedDate = new Date()
-      })
-    }
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
+      return setPropertyToNewDate('interruptedDate')
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
+      return setPropertyToNewDate('finishedDate')
     default:
       return state
   }
